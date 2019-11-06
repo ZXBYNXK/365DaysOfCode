ae4e418f71284f911779417258453c007714ef7c
DariusRain <dariusrain.student@careerdevs.com>
2019-11-06 18:57:14 -0500

    Day 11 of 365
    Created a simple node js program that predicts the number your thinking of.

---
+++ day11ofcode.js
@@ -0,0 +1,108 @@
+// Darius Rain                                              Day 11 of Code                     			         Date:11-6-19
+
+
+
+
+//		Todays project I will create a readline interface that predicts what number(1-10) your thinking of.
+//   Will add to this later on in 365 to make it predictor program in the end, started with something extremly simple.
+//   
+
+
+
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var startPrediction = function() {
+		
+		console.log(`STEP 1.  Think of a number....`)
+		console.log(`Press any key to continue...`)
+		rl.question(``, function(verify){
+		console.log(`STEP 2.  Multiply that number by 2`)
+		console.log(`Press any key to continue...`)
+		rl.question(``, function(verify){
+		console.log(`STEP 3.  Now add 10 more to it `)
+		console.log(`Press any key to continue...`);
+		rl.question(``, function(verify){
+		console.log(`STEP 4. Now divide it by 2`)
+		console.log(`Press any key to continue...`);
+		rl.question(``, function(verify) {
+		console.log(`FINAL STEP. Subtract the number you first thought of with the number now....`)
+		console.log(`Press any key and find out the computer's predicion.`)
+		rl.question(``, function(verify) {
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		
+		console.log(``);
+		console.log(``);
+		
+		console.log(``);
+		console.log(``);
+		console.log(``);
+		
+		console.log(`1011011010110101001110011010110--|_1_|--1010011110011010010011101011010010`)
+		console.log(`1001010100100010010011101010011--__2__--0011110010010100100101010101001101`)
+		console.log(`101001011101001010010010100100--___3___--100101001001001010010101011000111`)
+		console.log(`10010101010100010010101001110--____4____--10010101010010101111110110110110`)
+		console.log(`1010010101001010010101001001--_____x_____--1010100100111010101001001010101`)
+		console.log(`_____________________________|.....x.....|_______________________________`)
+		console.log(`_____________________________|.....*.....|_______________________________`)
+		console.log(`_____________________________|.....*.....|_______________________________`)
+		console.log(`_____________________________|.....*.....|_______________________________`)
+		console.log(`_____________________________|.....*.....|_______________________________`)
+		console.log(`_____________________________|.....*.....|_______________________________`)
+		console.log(`_____________________________|.....*.....|_______________________________`)
+		console.log(`_____________________________|.....*.....|_______________________________`)
+		console.log(`_____________________________|.....*.....|_______________________________`)
+		console.log(`_____________________________|....x=5....|_______________________________`)	
+		console.log(`The. Current Number.. Is 5. beeep boop beep....`)
+		})
+		})
+		})
+		})
+		})
+}
+startPrediction();
