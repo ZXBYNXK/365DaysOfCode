ffe64d39b4252536b5bf869d98ef2f18a1506b0e
DariusRain <dariusrain.student@careerdevs.com>
2019-12-28 19:48:09 -0500

    Find the least common multiple

--- day62/rest_api_v2/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http'),
-app = require('app')
-port = process.env.PORT || 5000,
-server = http.createServer(app);
-
-
-server.listen(port)
-console.log(`Listening on port ${port}`)
