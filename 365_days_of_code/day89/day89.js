c553e5c4c70882833c82e985986e47524d2fdd99
DariusRain <dariusrain.student@carrerdevs.com>
2020-01-23 22:08:42 -0500

    Getting familiar with Map()

---
+++ 365_days_of_code/day89/day89.js
@@ -0,0 +1,22 @@
+//Darius Rain
+//Day 89: Getting familiar with the features of map.
+
+let map = new Map();
+let ids = 0
+function genId() {
+ return (ids += 1).toString()
+
+}
+map.set(genId(), 'John')
+.set(genId(), 'Amy');
+
+console.log(map.get('1')); // v1
+
+console.log(map.has('2')); // true
+
+for (let ids of map.entries())
+  console.log(ids[0] + " : " + ids[1]);
+  
+  
+  
+  
